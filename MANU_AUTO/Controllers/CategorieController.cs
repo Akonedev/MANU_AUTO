838acb2b8c659dc41c5d44f919ff613208b16726
Akonedev <93835972+Akonedev@users.noreply.github.com>
2022-06-04 15:06:12 +0200

    Initial commit

---
+++ MANU_AUTO/Controllers/CategorieController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MANU_AUTO.Data;
+using MANU_AUTO.Models;
+using Microsoft.AspNetCore.Authorization;
+using PagedList;
+
+
+namespace MANU_AUTO.Controllers
+{
+    public class CategorieController : Controller
+    {
+        private readonly LAMANU_AUTOContext _context;
+
+        public CategorieController(LAMANU_AUTOContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Categorie
+        // [Authorize(Roles = "Admin, Employee")]
+        public async Task<IActionResult> Index()
+        {
+            return _context.Categories != null ?
+                        View(await _context.Categories.ToListAsync()) :
+                        Problem("Entity set 'ApplicationDbContext.Categorie'  is null.");
+        }
+
+        [HttpGet]
+        public IActionResult Index(int? id)
+        {
+            int pageSize = 4;
+            int pageIndex;
+
+            pageIndex = id.HasValue ? Convert.ToInt32(id.Value) : 1;
+
+            IPagedList<Categorie> cat = _context.Categories.OrderByDescending(c => c.Id).ToPagedList(pageIndex, pageSize);
+
+            return View(cat);
+        }
+
+        // GET: Categorie/Details/5
+        //[Authorize(Roles = "Admin, Employee")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var categorie = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categorie == null)
+            {
+                return NotFound();
+            }
+
+            return View(categorie);
+        }
+
+        // GET: Categorie/Create
+        //[Authorize(Roles = "Admin, Employee")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categorie/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[Authorize(Roles = "Admin, Employee")]
+        [HttpPost]
+       // [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Label")] Categorie categorie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(categorie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categorie);
+        }
+
+        // GET: Categorie/Edit/5
+        // [Authorize(Roles = "Admin, Employee")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var categorie = await _context.Categories.FindAsync(id);
+            if (categorie == null)
+            {
+                return NotFound();
+            }
+            return View(categorie);
+        }
+
+        // POST: Categorie/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
+        //[Authorize(Roles = "Admin, Employee")]        
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Label")] Categorie categorie)
+        {
+            if (id != categorie.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(categorie);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategorieExists(categorie.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categorie);
+        }
+
+        // GET: Categorie/Delete/5
+        // [Authorize(Roles = "Admin, Employee")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var categorie = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categorie == null)
+            {
+                return NotFound();
+            }
+
+            return View(categorie);
+        }
+
+        // POST: Categorie/Delete/5
+        // [Authorize(Roles = "Admin, Employee")]
+        [HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Categories == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Categorie'  is null.");
+            }
+            var categorie = await _context.Categories.FindAsync(id);
+            if (categorie != null)
+            {
+                _context.Categories.Remove(categorie);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategorieExists(int id)
+        {
+            return (_context.Categories?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
