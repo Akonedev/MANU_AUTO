7c331cd6a63d713cb7b7cc9f666ce3edcce31f4b
Akonedev <93835972+Akonedev@users.noreply.github.com>
2022-06-04 15:06:12 +0200

    fix links on index.html

---
+++ MANU_AUTO/Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MANU_AUTO.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("MANU_AUTO_IdentityContextConnection") ?? throw new InvalidOperationException("Connection string 'MANU_AUTO_IdentityContextConnection' not found.");
+
+builder.Services.AddDbContext<LAMANU_AUTOContext>(options =>
+    options.UseSqlServer(connectionString)); ;
+
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+        // Password settings.
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequiredLength = 6;
+        options.Password.RequiredUniqueChars = 1;
+
+        // Lockout settings.
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.AllowedForNewUsers = true;
+
+        // User settings.
+        options.User.AllowedUserNameCharacters =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+        options.User.RequireUniqueEmail = true;
+
+        //Email and Account Confirmation
+        //options.SignIn.RequireConfirmedAccount = false;
+        options.SignIn.RequireConfirmedEmail = false;
+        options.SignIn.RequireConfirmedPhoneNumber = false;
+    }
+
+)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<LAMANU_AUTOContext>();
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.MapRazorPages();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication(); ;
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
---
+++ MANU_AUTO/wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function visit(num) 
+
+{
+    if (sessionStorage.getItem(num) != null) {
+        document.getElementById("vst").innerHTML = "page visitée";
+        sessionStorage.setItem(num, 'visité');
+    }
+    else {
+        return false;
+    }
+       
+    //}
+    //document.getElementById("demo").innerHTML = sessionStorage.num;
+    console.log(sessionStorage.num);
+}
+
+function isVisited(nb) {
+    if (visit(nb) != null) {
+        document.getElementById("vst").innerHTML = "page visitée";
+        document.getElementById("vst").style.display = "block";
+    }
+}
